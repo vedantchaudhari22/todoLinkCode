4936f79dda3298f845601654431e28c7c959a8d9
Vedant Chaudhari <143114075+vedantchaudhari22@users.noreply.github.com>
2025-11-14 19:05:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "https://todo-frontend-pwix.onrender.com",
+    origin: "http://localhost:5173/",
     credentials: true,
   }));
 app.use(express.json());
