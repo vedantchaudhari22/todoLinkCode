20f8c75e6e4c00e4a459890849eca04cfd75eab9
Vedant Chaudhari <143114075+vedantchaudhari22@users.noreply.github.com>
2025-11-14 19:32:31 +0530

    Change API endpoint for fetching todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const getTodos = async () => {
-    const res = await axios.get("http://localhost:5000/todos");
+    const res = await axios.get("http://localhost:5000/gettodos");
     setTodos(res.data.todos);
   };
 
